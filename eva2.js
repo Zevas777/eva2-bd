84023627a74b483ec77e44d9b0bb60be93fa83fe
Zevas777 <sebas727sa@gmail.com>
2026-06-18 20:01:47 -0400

    Add files via upload

--- eva2.js
+++ eva2.js
@@ -10,24 +10,23 @@ db.usuariosyt.insertMany([
 {
 _id: 1,
 nombre: "Juan Perez",
-email: "juan@gmail.com",
-canal: {nombreCanal: "JuanGaming", suscriptores: 15000},
+email: "juan77@gmail.com",
+canal: {nombreCanal: "XxxJuanGamerxxX", suscriptores: 15},
 videos: [
-{titulo: "Mi primer video", visitas: 25000, likes: 3000, duracion: "10:35",
+{titulo: "Mi primer video", visitas: 2, likes: 1, duracion: "2:30",
 comentarios: [{usuario:"Pedro", mensaje:"Buen video!", likesComentario:25}]}
 ]
 },
 
 {
 _id: 2,
-nombre: "Luis Ramirez",
-email: "luis@gmail.com",
+nombre: "james donaldson,",
+email: "mrbeast@outlook.com",
 canal: {nombreCanal: "MrBeast", suscriptores: 400000000},
 videos: [
-{titulo:"El reto más grande del mundo", visitas:150000000, likes:8000000, duracion:"20:15",
-comentarios:[
-{usuario:"Ana", mensaje:"Increíble video", likesComentario:500}
-]}
+{titulo:"PUSE A 100 HOMELESS A PELEAR POR 500 MIL DOLARES!!!", visitas:150000000, likes:8000000, duracion:"20:15",
+comentarios:[{usuario:"Ana", mensaje:"Increíble video", likesComentario:500},
+    {usuario:"Luisxd", mensaje:"revivio el cine", likesComentario:300}]}
 ]
 },
 
@@ -38,48 +37,38 @@ email: "maria@gmail.com",
 canal: {nombreCanal:"CocinaFacil", suscriptores:85000},
 videos:[
 {titulo:"Receta de pizza casera", visitas:450000, likes:23000, duracion:"12:40",
-comentarios:[
-{usuario:"Carlos", mensaje:"La receta funciona", likesComentario:300}
-]}
+comentarios:[{usuario:"Carlos", mensaje:"La receta funciona", likesComentario:300}]}
 ]
 },
 
 {
 _id:4,
 nombre:"Pedro Gonzalez",
-email:"pedro@gmail.com",
+email:"pewdiepie@hotmail.com",
 canal:{nombreCanal:"PewDiePie", suscriptores:111000000},
-videos:[
-{titulo:"Gaming con amigos", visitas:9000000, likes:500000, duracion:"18:20",
-comentarios:[
-{usuario:"Jose",mensaje:"Muy entretenido",likesComentario:120}
-]}
+videos:[{titulo:"nueva temporada de fortnite", visitas:9000000, likes:500000, duracion:"18:20",
+comentarios:[{usuario:"Jose",mensaje:"Muy entretenido",likesComentario:120},
+    {usuario:"sigmaboy",mensaje:"like por la llama",likesComentario:10}]}
 ]
 },
 
 {
 _id:5,
 nombre:"Sofia Torres",
-email:"sofia@gmail.com",
+email:"sofivlogs@gmail.com",
 canal:{nombreCanal:"SofiaVlogs",suscriptores:12000},
-videos:[
-{titulo:"Mi viaje a Chile",visitas:35000,likes:2500,duracion:"14:10",
-comentarios:[
-{usuario:"Daniel",mensaje:"Hermoso lugar",likesComentario:50}
-]}
+videos:[{titulo:"Mi viaje a Chile",visitas:35000,likes:2500,duracion:"14:10",
+comentarios:[{usuario:"Daniel",mensaje:"Hermoso lugar",likesComentario:50}]}
 ]
 },
 
 {
 _id:6,
-nombre:"Diego Morales",
-email:"diego@gmail.com",
+nombre:"Ruben Dlobas",
+email:"elrubiusomg@gmail.com",
 canal:{nombreCanal:"ElRubius",suscriptores:40000000},
-videos:[
-{titulo:"Jugando Minecraft",visitas:7000000,likes:600000,duracion:"22:30",
-comentarios:[
-{usuario:"Mario",mensaje:"Que recuerdos",likesComentario:400}
-]}
+videos:[{titulo:"epic vlog 3",visitas:7000000,likes:600000,duracion:"15:00",
+comentarios:[{usuario:"Mario1213",mensaje:"Que recuerdos",likesComentario:400}]}
 ]
 },
 
@@ -88,11 +77,8 @@ _id:7,
 nombre:"Camila Diaz",
 email:"camila@gmail.com",
 canal:{nombreCanal:"AprendeProgramando",suscriptores:95000},
-videos:[
-{titulo:"Curso MongoDB básico",visitas:150000,likes:9000,duracion:"30:00",
-comentarios:[
-{usuario:"Luis",mensaje:"Muy explicado",likesComentario:200}
-]}
+videos:[{titulo:"Curso MongoDB básico",visitas:150000,likes:9000,duracion:"30:00",
+comentarios:[{usuario:"Luis",mensaje:"Muy explicado",likesComentario:200}]}
 ]
 },
 
@@ -101,11 +87,8 @@ _id:8,
 nombre:"Andres Silva",
 email:"andres@gmail.com",
 canal:{nombreCanal:"PintandoIdeas",suscriptores:25000},
-videos:[
-{titulo:"Dibujo paso a paso",visitas:80000,likes:6000,duracion:"16:25",
-comentarios:[
-{usuario:"Laura",mensaje:"Me encanto",likesComentario:80}
-]}
+videos:[{titulo:"Dibujo paso a paso",visitas:80000,likes:6000,duracion:"16:25",
+comentarios:[{usuario:"Laura",mensaje:"Me encanto",likesComentario:80}]}
 ]
 },
 
@@ -114,11 +97,8 @@ _id:9,
 nombre:"Valentina Rojas",
 email:"vale@gmail.com",
 canal:{nombreCanal:"TikTakChile",suscriptores:540000},
-videos:[
-{titulo:"Experimentos faciles",visitas:1200000,likes:45000,duracion:"09:50",
-comentarios:[
-{usuario:"Pedro",mensaje:"Buen experimento",likesComentario:150}
-]}
+videos:[{titulo:"Experimentos faciles",visitas:1200000,likes:45000,duracion:"09:50",
+comentarios:[{usuario:"Pedro",mensaje:"Buen experimento",likesComentario:150}]}
 ]
 },
 
@@ -127,11 +107,8 @@ _id:10,
 nombre:"Felipe Soto",
 email:"felipe@gmail.com",
 canal:{nombreCanal:"TechWorld",suscriptores:300000},
-videos:[
-{titulo:"Review de computadores",visitas:500000,likes:20000,duracion:"15:45",
-comentarios:[
-{usuario:"Ana",mensaje:"Me ayudo mucho",likesComentario:100}
-]}
+videos:[{titulo:"Review de computadores",visitas:500000,likes:20000,duracion:"15:45",
+comentarios:[{usuario:"Ana",mensaje:"Me ayudo mucho",likesComentario:100}]}
 ]
 }
 ])
